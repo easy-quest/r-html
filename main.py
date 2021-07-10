3f3e3e6e903edafbd0650ba4c9b3415dbc5f434d
Easy <easy-quest@mail.ru>
2021-07-10 20:35:59 +0000

    3 changed files or folders

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import requests
 from auth_data import token
 
 group_name = input("Введите название группы: ")
-url = "https://api.vk.com/method/wall.get?domain={group_name}&count=40&access_token={token}&v=5.52"
-print(url)
+
+url = f"https://api.vk.com/method/wall.get?domain={group_name}&count=40&access_token={token}&v=5.52"
+req = requests.get(url)
+print(req.text)
